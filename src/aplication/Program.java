c239f3e2357d6ea47def9738ed71880c5fa9332c
Tiago Costa <tiagocostasantos0013@gmail.com>
2025-07-15 20:41:58 -0300

    Seller entity class

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,13 +1,17 @@
 package aplication;
 
+import java.sql.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(321, "bob", "bob@gmail.com", new Date(0), 3000.0, obj);
+		System.out.println(seller);
 	}
 
 }
